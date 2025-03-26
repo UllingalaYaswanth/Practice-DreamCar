57cc29c23c04459e01b6d94a87ce76c7f02a4b93
UllingalaYaswanth <yaswanthullingala@gmail.com>
2025-03-26 10:26:37 +0530

    added supabase and arcjet for db and security

---
+++ app/waitlist/page.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Waitlist() {
+  return (
+    <div className='mt-20 w-full'>
+        <div className='aspect-video w-full'>
+        <iframe style={{width: '100%'}} height="950" src="https://www.create.xyz/app/5db8bb19-f765-4a36-8894-e46c56990f28" title="Waiting List Landing Page" frameBorder="0"></iframe>
+        </div>
+    </div>
+  )
+}
+
+export default Waitlist
